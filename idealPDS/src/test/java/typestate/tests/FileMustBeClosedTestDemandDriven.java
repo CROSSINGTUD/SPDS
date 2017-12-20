cbea181270b6a3f751b00c0f161d03e1e1959540
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2017-12-20 13:05:33 +0100

    Rename tests according which analysis would detect each case

--- idealPDS/src/test/java/typestate/tests/FileMustBeClosedTestDemandDriven.java
+++ idealPDS/src/test/java/typestate/tests/FileMustBeClosedTestDemandDriven.java
@@ -10,24 +10,35 @@ import typestate.test.helper.ObjectWithField;
 
 public class FileMustBeClosedTestDemandDriven extends IDEALTestingFramework{
 	@Test
-	public void simple() {
+	public void notCaughtByCHA() {
 		File file = new File();
 		file.open();
 		B b = new B();
 		b.flow(file);
 		mustBeInAcceptingState(file);
 	}
-	
-	private static interface I{
+
+	@Test
+	public void notCaughtByRTA() {
+		File file = new File();
+		file.open();
+		A a = new A();
+		B b = new B();
+		b.flow(file);
+		mustBeInAcceptingState(file);
+	}
+
+	private interface I{
 		void flow(File f);
 	}
-	
+
 	private static class B implements I{
 		@Override
 		public void flow(File f) {
 			f.close();
 		}
 	}
+
 	private static class A implements I{
 		@Override
 		public void flow(File f) {
