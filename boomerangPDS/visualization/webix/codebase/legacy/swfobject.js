e46f75ace640811f3fd90541b634976e72055c81
Johannes Späth <joh.spaeth@googlemail.com>
2018-01-22 18:51:57 +0100

    Added copyright and license

--- boomerangPDS/visualization/webix/codebase/legacy/swfobject.js
+++ boomerangPDS/visualization/webix/codebase/legacy/swfobject.js
@@ -1,6 +1,6 @@
 /*!	SWFObject v2.2 <http://code.google.com/p/swfobject/> 
 
-Copyright (c) 2009 bobbyvanderluis and TenSafeF...@gmail.com
+Copyright (c) 2009, 2018 bobbyvanderluis and TenSafeF...@gmail.com
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- boomerangPDS/visualization/webix/codebase/webix_debug.js
+++ boomerangPDS/visualization/webix/codebase/webix_debug.js
@@ -1101,7 +1101,7 @@ https://github.com/Zolmeister/promiz
 
 The MIT License (MIT)
 
-Copyright (c) 2014 Zolmeister
+Copyright (c) 2014, 2018 Zolmeister
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
