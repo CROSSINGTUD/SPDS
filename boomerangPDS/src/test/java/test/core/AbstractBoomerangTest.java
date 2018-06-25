4a29c29293a11d01cec6a178d28e367189d018b1
Melanie Bruns <mbruns42@mail.uni-paderborn.de>
2018-06-25 10:16:05 +0200

    Only turn on demand driven analysis in this for dynamic icfg

--- boomerangPDS/src/test/java/test/core/AbstractBoomerangTest.java
+++ boomerangPDS/src/test/java/test/core/AbstractBoomerangTest.java
@@ -79,7 +79,6 @@ public class AbstractBoomerangTest extends AbstractTestingFramework {
 
 	protected AnalysisMode[] getAnalyses() {
 		return new AnalysisMode[] {
-				 AnalysisMode.WholeProgram,
 				AnalysisMode.DemandDrivenBackward
 				};
 	}
