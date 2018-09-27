7d555999b011a599f93e9cfc8aa5d49b8bf2940d
Johannes Späth <joh.spaeth@googlemail.com>
2018-09-27 14:05:06 +0200

    Moved inner class ExtractAllAliasListener to new class

--- boomerangPDS/src/main/java/boomerang/results/BackwardBoomerangResults.java
+++ boomerangPDS/src/main/java/boomerang/results/BackwardBoomerangResults.java
@@ -27,7 +27,6 @@ import soot.PointsToSet;
 import soot.Type;
 import soot.jimple.ClassConstant;
 import soot.jimple.NewExpr;
-import sync.pds.solver.SyncPDSUpdateListener;
 import sync.pds.solver.nodes.GeneratedState;
 import sync.pds.solver.nodes.INode;
 import sync.pds.solver.nodes.Node;
@@ -38,7 +37,6 @@ import wpds.impl.Weight;
 import wpds.impl.WeightedPAutomaton;
 import wpds.interfaces.Empty;
 import wpds.interfaces.WPAStateListener;
-import wpds.interfaces.WPAUpdateListener;
 
 public class BackwardBoomerangResults<W extends Weight> extends AbstractBoomerangResults<W> implements PointsToSet{
 
@@ -161,34 +159,7 @@ public class BackwardBoomerangResults<W extends Weight> extends AbstractBoomeran
 		for (final Query fw : getAllocationSites().keySet()) {
 			if(fw instanceof BackwardQuery)
 				continue;
-			queryToSolvers.getOrCreate(fw).registerListener(new SyncPDSUpdateListener<Statement, Val>() {
-				
-				@Override
-				public void onReachableNodeAdded(Node<Statement, Val> reachableNode) {
-					if(reachableNode.stmt().equals(stmt)){
-						Val base = reachableNode.fact();
-						final INode<Node<Statement, Val>> allocNode = queryToSolvers.getOrCreate(fw).getFieldAutomaton().getInitialState();			
-						queryToSolvers.getOrCreate(fw).getFieldAutomaton().registerListener(new WPAUpdateListener<Field, INode<Node<Statement, Val>>, W>() {
-							@Override
-							public void onWeightAdded(Transition<Field, INode<Node<Statement, Val>>> t, W w,
-									WeightedPAutomaton<Field, INode<Node<Statement, Val>>, W> aut) {
-								if(t.getStart().fact().stmt().equals(stmt) && !(t.getStart() instanceof GeneratedState) && t.getStart().fact().fact().equals(base)){
-									if (t.getLabel().equals(Field.empty())) {
-										if (t.getTarget().equals(allocNode)) {
-											results.add(new AccessPath(base));
-										}
-									}
-									List<Transition<Field, INode<Node<Statement, Val>>>> fields = Lists.newArrayList();
-									if (!(t.getLabel() instanceof Empty)) {
-										fields.add(t);
-									}
-									queryToSolvers.getOrCreate(fw).getFieldAutomaton().registerListener(new ExtractAccessPathStateListener(t.getTarget(),allocNode,base, fields, results));
-								}
-							}
-						});
-					}
-				}
-			});
+			queryToSolvers.getOrCreate(fw).registerListener(new ExtractAllAliasListener<W>(this.queryToSolvers.get(fw), results, stmt));
 			
 		}
 		return results;
@@ -197,106 +168,8 @@ public class BackwardBoomerangResults<W extends Weight> extends AbstractBoomeran
 	public Set<AccessPath> getAllAliases() {
 		return getAllAliases(query.stmt());
 	}
-	private class ExtractAccessPathStateListener extends WPAStateListener<Field, INode<Node<Statement, Val>>, W> {
-
-		private INode<Node<Statement, Val>> allocNode;
-		private Collection<Transition<Field, INode<Node<Statement, Val>>>> fields;
-		private Set<AccessPath> results;
-		private Val base;
-
-		public ExtractAccessPathStateListener(INode<Node<Statement, Val>> state, INode<Node<Statement, Val>> allocNode,
-				Val base, Collection<Transition<Field, INode<Node<Statement, Val>>>> fields, Set<AccessPath> results) {
-			super(state);
-			this.allocNode = allocNode;
-			this.base = base;
-			this.fields = fields;
-			this.results = results;
-		}
-
-		@Override
-		public void onOutTransitionAdded(Transition<Field, INode<Node<Statement, Val>>> t, W w,
-				WeightedPAutomaton<Field, INode<Node<Statement, Val>>, W> weightedPAutomaton) {
-			if(t.getLabel().equals(Field.epsilon()))
-				return;
-			Collection<Transition<Field, INode<Node<Statement, Val>>>> copiedFields = (fields instanceof Set ? Sets.newHashSet(fields) : Lists.newArrayList(fields));
-			if (!t.getLabel().equals(Field.empty())) {
-				if(copiedFields.contains(t)){
-					copiedFields = Sets.newHashSet(fields);
-				}
-				if(!(t.getLabel() instanceof Empty))
-					copiedFields.add(t);
-			}
-			if (t.getTarget().equals(allocNode)) {
-				
-				results.add(new AccessPath(base, convert(copiedFields)));
-			} 
-			weightedPAutomaton.registerListener(
-							new ExtractAccessPathStateListener(t.getTarget(), allocNode, base, copiedFields, results));
-		}
-
-		private Collection<Field> convert(Collection<Transition<Field, INode<Node<Statement, Val>>>> fields) {
-			Collection<Field> res;
-			if(fields instanceof List) {
-				res = Lists.newArrayList();
-			} else {
-				res = Sets.newHashSet();
-			}
-			for(Transition<Field, INode<Node<Statement, Val>>> f : fields) {
-				res.add(f.getLabel());
-			}
-			return res;
-		}
-
-		@Override
-		public void onInTransitionAdded(Transition<Field, INode<Node<Statement, Val>>> t, W w,
-				WeightedPAutomaton<Field, INode<Node<Statement, Val>>, W> weightedPAutomaton) {
-		}
-
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = super.hashCode();
-			result = prime * result + getOuterType().hashCode();
-			result = prime * result + ((allocNode == null) ? 0 : allocNode.hashCode());
-			result = prime * result + ((base == null) ? 0 : base.hashCode());
-//			result = prime * result + ((fields == null) ? 0 : fields.hashCode());
-			return result;
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (!super.equals(obj))
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			ExtractAccessPathStateListener other = (ExtractAccessPathStateListener) obj;
-			if (!getOuterType().equals(other.getOuterType()))
-				return false;
-			if (allocNode == null) {
-				if (other.allocNode != null)
-					return false;
-			} else if (!allocNode.equals(other.allocNode))
-				return false;
-			if (base == null) {
-				if (other.base != null)
-					return false;
-			} else if (!base.equals(other.base))
-				return false;
-//			if (fields == null) {
-//				if (other.fields != null)
-//					return false;
-//			} else if (!fields.equals(other.fields))
-//				return false;
-			return true;
-		}
-
-		private BackwardBoomerangResults getOuterType() {
-			return BackwardBoomerangResults.this;
-		}
-
-	}
+	
+	
 
 	
 	@Override
