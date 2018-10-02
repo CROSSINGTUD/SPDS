a10d68766caf5d3284ac1fc5709194251c262ecf
Johannes Spaeth <joh.spaeth@googlemail.com>
2018-10-02 09:26:07 +0200

    Additional check for Seed to be call graph reachable

--- boomerangPDS/src/main/java/boomerang/seedfactory/SeedFactory.java
+++ boomerangPDS/src/main/java/boomerang/seedfactory/SeedFactory.java
@@ -44,71 +44,72 @@ import wpds.interfaces.WPAUpdateListener;
  */
 public abstract class SeedFactory<W extends Weight> {
 
-    private final WeightedPushdownSystem<Method, INode<Reachable>, Weight.NoWeight> pds = new WeightedPushdownSystem<>();
-    private final Multimap<Query,Transition<Method, INode<Reachable>>> seedToTransition = HashMultimap.create();
-    private final Multimap<SootMethod, Query> seedsPerMethod = HashMultimap.create();
-    
-    private final WeightedPAutomaton<Method,  INode<Reachable>, Weight.NoWeight> automaton = new WeightedPAutomaton<Method,  INode<Reachable>, Weight.NoWeight>(wrap(Reachable.entry())) {
-        @Override
-        public INode<Reachable> createState(INode<Reachable> reachable, Method loc) {
-            return new GeneratedState<>(reachable,loc);
-        }
+	private final WeightedPushdownSystem<Method, INode<Reachable>, Weight.NoWeight> pds = new WeightedPushdownSystem<>();
+	private final Multimap<Query, Transition<Method, INode<Reachable>>> seedToTransition = HashMultimap.create();
+	private final Multimap<SootMethod, Query> seedsPerMethod = HashMultimap.create();
 
-        @Override
-        public boolean isGeneratedState(INode<Reachable> reachable) {
-            return reachable instanceof GeneratedState;
-        }
+	private final WeightedPAutomaton<Method, INode<Reachable>, Weight.NoWeight> automaton = new WeightedPAutomaton<Method, INode<Reachable>, Weight.NoWeight>(
+			wrap(Reachable.entry())) {
+		@Override
+		public INode<Reachable> createState(INode<Reachable> reachable, Method loc) {
+			return new GeneratedState<>(reachable, loc);
+		}
 
+		@Override
+		public boolean isGeneratedState(INode<Reachable> reachable) {
+			return reachable instanceof GeneratedState;
+		}
 
-        @Override
-        public Method epsilon() {
-            return Method.epsilon();
-        }
+		@Override
+		public Method epsilon() {
+			return Method.epsilon();
+		}
 
-        @Override
-        public Weight.NoWeight getZero() {
-            return Weight.NO_WEIGHT_ZERO;
-        }
+		@Override
+		public Weight.NoWeight getZero() {
+			return Weight.NO_WEIGHT_ZERO;
+		}
 
-        @Override
-        public Weight.NoWeight getOne() {
-            return Weight.NO_WEIGHT_ONE;
-        }
-    };
+		@Override
+		public Weight.NoWeight getOne() {
+			return Weight.NO_WEIGHT_ONE;
+		}
+	};
 	private Collection<SootMethod> processed = Sets.newHashSet();
 	private Multimap<Query, SootMethod> queryToScope = HashMultimap.create();
 
-    public Collection<Query> computeSeeds(){
-        List<SootMethod> entryPoints = Scene.v().getEntryPoints();
-        for(SootMethod m : entryPoints){
-            automaton.addTransition(new Transition<>(wrap(Reachable.v()),new Method(m),automaton.getInitialState()));
-        }
-        pds.poststar(automaton);
-        automaton.registerListener(new WPAUpdateListener<Method, INode<Reachable>, Weight.NoWeight>() {
-            @Override
-            public void onWeightAdded(Transition<Method, INode<Reachable>> t, Weight.NoWeight noWeight, WeightedPAutomaton<Method, INode<Reachable>, Weight.NoWeight> aut) {
-                process(t);
-            }
-        });
-        
-        if(analyseClassInitializers()){
-        	Set<SootClass> sootClasses = Sets.newHashSet();
-        	for(SootMethod p : Sets.newHashSet(processed)){
-        		if(sootClasses.add(p.getDeclaringClass())){
-        			addStaticInitializerFor(p.getDeclaringClass());
-        		}
-        	}
-        }
-        
-        return seedToTransition.keySet();
-    }
+	public Collection<Query> computeSeeds() {
+		List<SootMethod> entryPoints = Scene.v().getEntryPoints();
+		for (SootMethod m : entryPoints) {
+			automaton.addTransition(new Transition<>(wrap(Reachable.v()), new Method(m), automaton.getInitialState()));
+		}
+		pds.poststar(automaton);
+		automaton.registerListener(new WPAUpdateListener<Method, INode<Reachable>, Weight.NoWeight>() {
+			@Override
+			public void onWeightAdded(Transition<Method, INode<Reachable>> t, Weight.NoWeight noWeight,
+					WeightedPAutomaton<Method, INode<Reachable>, Weight.NoWeight> aut) {
+				process(t);
+			}
+		});
 
-    private void addStaticInitializerFor(SootClass declaringClass) {
-		for(SootMethod m : declaringClass.getMethods()){
-			if(m.isStaticInitializer()){
-		        for(SootMethod ep : Scene.v().getEntryPoints()){
-		        	addPushRule(new Method(ep), new Method(m));
-		        }
+		if (analyseClassInitializers()) {
+			Set<SootClass> sootClasses = Sets.newHashSet();
+			for (SootMethod p : Sets.newHashSet(processed)) {
+				if (sootClasses.add(p.getDeclaringClass())) {
+					addStaticInitializerFor(p.getDeclaringClass());
+				}
+			}
+		}
+
+		return seedToTransition.keySet();
+	}
+
+	private void addStaticInitializerFor(SootClass declaringClass) {
+		for (SootMethod m : declaringClass.getMethods()) {
+			if (m.isStaticInitializer()) {
+				for (SootMethod ep : Scene.v().getEntryPoints()) {
+					addPushRule(new Method(ep), new Method(m));
+				}
 			}
 		}
 	}
@@ -117,63 +118,67 @@ public abstract class SeedFactory<W extends Weight> {
 		return false;
 	}
 
-	protected abstract Collection<? extends Query> generate(SootMethod method, Stmt u, Collection<SootMethod> calledMethods);
-	
-    private void process(Transition<Method, INode<Reachable>> t) {
-        Method curr = t.getLabel();
-        SootMethod m = curr.getMethod();
-    	if(!m.hasActiveBody())
-    	    return;
-    	computeQueriesPerMethod(m);
-    	for(Query q : seedsPerMethod.get(m)){
-    		seedToTransition.put(q, t);
-    	}
-    }
+	protected abstract Collection<? extends Query> generate(SootMethod method, Stmt u,
+			Collection<SootMethod> calledMethods);
 
-    private void computeQueriesPerMethod(SootMethod m) {
-    	if(!processed.add(m)) {
-    		return;
-    	}
-    	Set<Query> seeds = Sets.newHashSet();
-    	for(Unit u : m.getActiveBody().getUnits()) {
-            Collection<SootMethod> calledMethods = (icfg().isCallStmt(u) ? icfg().getCalleesOfCallAt(u)
-                    : new HashSet<SootMethod>());
-            seeds.addAll(generate(m, (Stmt) u, calledMethods));
-            if (icfg().isCallStmt(u)) {
-                for (SootMethod callee : icfg().getCalleesOfCallAt(u)) {
-                    if (!callee.hasActiveBody())
-                        continue;
-                    addPushRule(new Method(m),new Method(callee));
-                }
-            }
-        }
-    	seedsPerMethod.putAll(m, seeds);
+	private void process(Transition<Method, INode<Reachable>> t) {
+		Method curr = t.getLabel();
+		SootMethod m = curr.getMethod();
+		if (!m.hasActiveBody())
+			return;
+		computeQueriesPerMethod(m);
+		for (Query q : seedsPerMethod.get(m)) {
+			seedToTransition.put(q, t);
+		}
+	}
+
+	private void computeQueriesPerMethod(SootMethod m) {
+		if (!processed.add(m)) {
+			return;
+		}
+		Set<Query> seeds = Sets.newHashSet();
+		for (Unit u : m.getActiveBody().getUnits()) {
+			if(!icfg().isReachable(u))
+				continue;
+			Collection<SootMethod> calledMethods = (icfg().isCallStmt(u) ? icfg().getCalleesOfCallAt(u)
+					: new HashSet<SootMethod>());
+			seeds.addAll(generate(m, (Stmt) u, calledMethods));
+			if (icfg().isCallStmt(u)) {
+				for (SootMethod callee : icfg().getCalleesOfCallAt(u)) {
+					if (!callee.hasActiveBody())
+						continue;
+					addPushRule(new Method(m), new Method(callee));
+				}
+			}
+		}
+		seedsPerMethod.putAll(m, seeds);
 	}
 
 	private void addPushRule(Method caller, Method callee) {
-        pds.addRule(new PushRule<>(wrap(Reachable.v()),caller,wrap(Reachable.v()),callee,caller, Weight.NO_WEIGHT_ONE));
-    }
+		pds.addRule(
+				new PushRule<>(wrap(Reachable.v()), caller, wrap(Reachable.v()), callee, caller, Weight.NO_WEIGHT_ONE));
+	}
 
-    private INode<Reachable> wrap(Reachable r){
-        return new SingleNode<>(r);
-    }
+	private INode<Reachable> wrap(Reachable r) {
+		return new SingleNode<>(r);
+	}
 
-    public abstract BiDiInterproceduralCFG<Unit,SootMethod> icfg();
+	public abstract BiDiInterproceduralCFG<Unit, SootMethod> icfg();
 
 	public Collection<SootMethod> getMethodScope(Query query) {
 		Set<SootMethod> scope = Sets.newHashSet();
-		if(queryToScope.containsKey(query)){
+		if (queryToScope.containsKey(query)) {
 			return queryToScope.get(query);
 		}
-		for(Transition<Method, INode<Reachable>> t : seedToTransition.get(query)){
+		for (Transition<Method, INode<Reachable>> t : seedToTransition.get(query)) {
 			scope.add(t.getLabel().getMethod());
 			automaton.registerListener(new TransitiveClosure(t.getTarget(), scope, query));
 		}
 		queryToScope.putAll(query, scope);
-		return scope;		
+		return scope;
 	}
 
-	private class TransitiveClosure extends WPAStateListener<Method, INode<Reachable>, Weight.NoWeight>{
+	private class TransitiveClosure extends WPAStateListener<Method, INode<Reachable>, Weight.NoWeight> {
 
 		private final Set<SootMethod> scope;
 		private Query query;
@@ -194,7 +199,7 @@ public abstract class SeedFactory<W extends Weight> {
 		@Override
 		public void onInTransitionAdded(Transition<Method, INode<Reachable>> t, NoWeight w,
 				WeightedPAutomaton<Method, INode<Reachable>, NoWeight> weightedPAutomaton) {
-			
+
 		}
 
 		@Override
@@ -229,6 +234,5 @@ public abstract class SeedFactory<W extends Weight> {
 			return SeedFactory.this;
 		}
 	}
-
 
 }
