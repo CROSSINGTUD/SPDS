697fdb98a0a7b8382f2c3aee6e2c8261ff1a08cd
Johannes Späth <joh.spaeth@googlemail.com>
2019-02-26 10:25:31 +0100

    Minor improvement

--- boomerangPDS/src/main/java/boomerang/seedfactory/SeedFactory.java
+++ boomerangPDS/src/main/java/boomerang/seedfactory/SeedFactory.java
@@ -154,7 +154,7 @@ public abstract class SeedFactory<W extends Weight> {
 
 					@Override
 					public void onCalleeAdded(Unit n, SootMethod callee) {
-						  if (!callee.hasActiveBody() || !Scene.v().getApplicationClasses().contains(callee.getDeclaringClass()))
+						  if (!callee.hasActiveBody() || !callee.getDeclaringClass().isApplicationClass())
 		                        return;
 						  addPushRule(new Method(m),new Method(callee));
 					}
